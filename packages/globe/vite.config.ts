2692f30bafc6a247a6952adb6a86cdcd71d7096e
yupeng <1196194875@qq.com>
2026-03-10 12:44:16 +0800

    chore: scaffold @vellusion/globe package

---
+++ packages/globe/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['@vellusion/math', '@vellusion/core', '@vellusion/scene'],
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
+});
